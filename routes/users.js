a97f2a3c06b7b0ddf99e20e7853965e5da608adf
Andi Rustianto <anditodev46@gmail.com>
2022-08-29 09:09:56 +0700

    Add: User Model

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-const usersController = require("../src/controllers/users");
+const usersController = require("../src/controllers/Users");
 
 /* GET users listing. */
 router.get("/", usersController.getAllUsers);
